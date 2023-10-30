0b1fae55719c896e58be0bbfcc72e7d16c9250cc
shaheen2013 <coredeveloper.2013@gmail.com>
2023-10-30 21:13:01 +0600

    added mark as completion dropdown

--- resources/js/components/TaskColumn.vue
+++ resources/js/components/TaskColumn.vue
@@ -5,9 +5,56 @@
                 <h2 class="text-lg text-zinc-100 font-black mb-3">{{ kanban.phases[props.phase_id].name }}</h2>
 
                 <h4 class="mb-3 h-6 w-6 right-0 text-white border-1 border-red-400 rounded" aria-hidden="true"> {{ kanban.phases[props.phase_id].tasks.length }}</h4>
-                <CheckIcon class="mb-3 h-6 w-6 right-0 text-white hover:cursor-pointer" aria-hidden="true" />
 
                 <PlusIcon @click="createTask()" class="mb-3 h-6 w-6 text-white hover:cursor-pointer" aria-hidden="true" />
+
+                    <div class="w-56 text-right">
+                        <Menu as="div" class="relative inline-block text-left">
+                            <div>
+                                <MenuButton
+                                    class="inline-flex w-full justify-center rounded-md px-4 pb-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                                    </svg>
+
+                                </MenuButton>
+                            </div>
+
+                            <transition
+                                enter-active-class="transition duration-100 ease-out"
+                                enter-from-class="transform scale-95 opacity-0"
+                                enter-to-class="transform scale-100 opacity-100"
+                                leave-active-class="transition duration-75 ease-in"
+                                leave-from-class="transform scale-100 opacity-100"
+                                leave-to-class="transform scale-95 opacity-0"
+                            >
+                                <MenuItems
+                                    class="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none z-50"
+
+                                >
+                                    <div class="px-1 py-1">
+                                        <MenuItem v-slot="{ active }">
+                                            <button
+                                                :class="[
+                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                ]"
+                                            >
+                                                <CheckIcon
+                                                    :active="active"
+                                                    class="mr-2 h-5 w-5 text-violet-400"
+                                                    aria-hidden="true"
+                                                />
+                                                Mark as Completion phase
+                                            </button>
+                                        </MenuItem>
+
+                                    </div>
+                                </MenuItems>
+                            </transition>
+                        </Menu>
+                    </div>
             </div>
             <task-card v-if="kanban.phases[props.phase_id].tasks.length > 0"
                 v-for="task in kanban.phases[props.phase_id].tasks" :task="task" />
@@ -26,6 +73,8 @@
 // get the props
 import { useKanbanStore } from '../stores/kanban'
 import { PlusIcon , CheckIcon } from '@heroicons/vue/20/solid'
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+
 
 const kanban = useKanbanStore()
 
